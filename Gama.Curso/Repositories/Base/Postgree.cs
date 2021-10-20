99782e51afeb4cce18bbbe996b7381ab4514d17c
Willian dos Santos <williansantos465@hotmail.com>
2021-10-20 19:48:48 -0300

    Conexão base do banco

---
+++ Gama.Curso/Repositories/Base/Postgree.cs
@@ -0,0 +1,29 @@
+﻿using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Gama.Curso.Repositories.Base
+{
+    public class Postgree
+    {
+        internal IDbConnection GetConnection()
+        {
+            IDbConnection dbConnection;
+
+            try
+            {
+                dbConnection = new NpgsqlConnection(
+                    "User ID = postgres; Password = Am!nA!2O2; Host = dindin.c1rfboikfug7.us-east-1.rds.amazonaws.com; Port = 5432; Database = cursosdd");
+                dbConnection.Open();
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+            return dbConnection;
+        }
+    }
+}
