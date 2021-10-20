b5b750ff279625e418b7a4d519928a0f26424013
Willian dos Santos <williansantos465@hotmail.com>
2021-10-20 19:42:58 -0300

    Estruturando projeto

---
+++ Gama.Curso/Extensions/DI/RegistrationDependencyInjection.cs
@@ -0,0 +1,31 @@
+﻿
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Conta.Extensions.DI
+{
+    public static class RegistrationDependencyInjection
+    {
+        /// <summary>
+        /// Método para realizar o registro das dependências
+        /// </summary>
+        /// <param name="services"></param>
+        /// <param name="configuration"></param>
+        public static void RegistrarDependencias(this IServiceCollection services, IConfiguration configuration = null)
+        {
+            //services.AddSingleton<IConverter, Coverter>();
+            ResgistrarRepositories(services);
+            ResgistrarServices(services);
+        }
+
+        public static void ResgistrarRepositories(IServiceCollection services)
+        {
+            
+        }
+
+        public static void ResgistrarServices(IServiceCollection services)
+        {
+            
+        }
+    }
+}
--- Gama.Curso/Startup.cs
+++ Gama.Curso/Startup.cs
@@ -1,15 +1,18 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
-using System.Threading.Tasks;
+using System.Reflection;
+using Conta.Extensions.DI;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Versioning;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 
 namespace Gama.Curso
 {
@@ -17,15 +20,40 @@ namespace Gama.Curso
     {
         public Startup(IConfiguration configuration)
         {
-            Configuration = configuration;
+            _configuration = configuration;
         }
 
-        public IConfiguration Configuration { get; }
+        public IConfiguration _configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+            services.AddCors(options =>
+            options.AddDefaultPolicy(
+                builder => builder.AllowAnyOrigin()));
+
+            // registro das dependências
+            services.RegistrarDependencias(_configuration);
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Conta", Version = "v1" });
+                string xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                string xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                c.IncludeXmlComments(xmlPath);
+            });
+
+            services.AddApiVersioning(options =>
+            {
+                options.DefaultApiVersion = new ApiVersion(1, 0);
+                options.ReportApiVersions = true;
+                options.AssumeDefaultVersionWhenUnspecified = true;
+                options.ApiVersionReader = new QueryStringApiVersionReader();
+                options.ApiVersionSelector = new CurrentImplementationApiVersionSelector(options);
+            });
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -45,6 +73,25 @@ namespace Gama.Curso
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
+            });
+
+            UseSwagger(app, "");
+
+        }
+        private void UseSwagger(IApplicationBuilder app, string routerPrefix)
+        {
+            app.UseSwagger(
+                c => c.PreSerializeFilters.Add((swaggerDoc, httpReq) =>
+                {
+                    swaggerDoc.Servers = new List<OpenApiServer> { new OpenApiServer { Url = $"{httpReq.Scheme}://{httpReq.Host.Value}{routerPrefix}" },
+                                                                   new OpenApiServer { Url = $"https://{httpReq.Host.Value}{routerPrefix}" }
+                                                                 };
+
+                }));
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint($"{routerPrefix}/swagger/v1/swagger.json", $"{_configuration.GetSection("Application").GetValue<string>("EndpointName")}");
+                c.RoutePrefix = string.Empty;
             });
         }
     }
