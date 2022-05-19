478a967907039dd7ff280ceeb95e103273c46796
Willian dos Santos <willians@animaeducacao.com.br>
2022-05-18 23:20:36 -0300

    Retirando dados de conexão com aws

--- Gama.Curso/Repositories/Base/Postgree.cs
+++ Gama.Curso/Repositories/Base/Postgree.cs
@@ -14,10 +14,8 @@ namespace Gama.Curso.Repositories.Base
             IDbConnection dbConnection;
 
             try
-            {
-
-                dbConnection = new NpgsqlConnection("User ID = xacesso; Password = bancositdis; Host = rds-sisdis.cli1aipb13fa.sa-east-1.rds.amazonaws.com; Port = 5432; Database = dbcurso");
-                //dbConnection = new NpgsqlConnection("User ID = postgres; Password = banco123; Host = localhost; Port = 5432; Database = curso");
+            {                
+                dbConnection = new NpgsqlConnection("User ID = postgres; Password = banco123; Host = localhost; Port = 5432; Database = curso");
                 dbConnection.Open();
             }
             catch (Exception e)
