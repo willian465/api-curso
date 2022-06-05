ee1e85c7cd88cfbbc6545939d4d01c86fdd9093b
Willian dos Santos <willians@animaeducacao.com.br>
2022-06-05 10:08:54 -0300

    Alteracao de estrutura

--- Repositories/Base/Postgree.cs
+++ Repositories/Base/Postgree.cs
@@ -15,7 +15,8 @@ namespace Gama.Curso.Repositories.Base
 
             try
             {                
-                dbConnection = new NpgsqlConnection("User ID = postgres; Password = banco123; Host = localhost; Port = 5432; Database = curso");
+                //dbConnection = new NpgsqlConnection("User ID = postgres; Password = banco123; Host = localhost; Port = 5432; Database = curso");
+                dbConnection = new NpgsqlConnection("User ID = xacesso; Password = bancositdis; Host = rds-sisdis.cli1aipb13fa.sa-east-1.rds.amazonaws.com; Port = 5432; Database = dbcurso");
                 dbConnection.Open();
             }
             catch (Exception e)
